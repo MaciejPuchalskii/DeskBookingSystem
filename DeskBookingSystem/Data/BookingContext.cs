e657dba4b9432b60681cf67449a9c251e753a1c5
Maciej Puchalski <maciejpe34@gmail.com>
2024-10-12 08:45:44 +0200

    Create BookingContext

---
+++ DeskBookingSystem/Data/BookingContext.cs
@@ -0,0 +1,25 @@
+﻿using DeskBookingSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeskBookingSystem.Data
+{
+    public class BookingContext : DbContext
+    {
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Desk> Desks { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        public BookingContext(DbContextOptions<BookingContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=bookingDeskSystem.db");
+            }
+        }
+    }
+}
