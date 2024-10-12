68e10af68e619750937da408e4744bc503e0b444
Maciej Puchalski <maciejpe34@gmail.com>
2024-10-12 08:45:29 +0200

    Add models for desk, employee, location and reservation

---
+++ DeskBookingSystem/Models/Desk.cs
@@ -0,0 +1,14 @@
+﻿namespace DeskBookingSystem.Models
+{
+    public class Desk
+    {
+        public int Id { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public int LocationId { get; set; }
+
+        public Location Location { get; set; }
+
+        public List<Reservation> Reservations { get; set; } = new List<Reservation>();
+    }
+}
---
+++ DeskBookingSystem/Models/Reservation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeskBookingSystem.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public DateTime BookingDate { get; set; }
+        public DateTime ReservationDate { get; set; }
+
+        [Range(1, 7, ErrorMessage = "Reservation cannot exceed 7 days.")]
+        public int HowManyDays { get; set; }
+
+        public int DeskId { get; set; }
+        public Desk Desk { get; set; }
+
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
