c0f8b444b0466bbd5d39c5baedab1cae0fccdafa
Maciej Puchalski <maciejpe34@gmail.com>
2024-10-13 09:41:25 +0200

    Add JWT authorization

---
+++ DeskBookingSystem/Controllers/AuthController.cs
@@ -0,0 +1,97 @@
+﻿using DeskBookingSystem.Data;
+using DeskBookingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DeskBookingSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly BookingContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(BookingContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(RegisterDto requestUser)
+        {
+            if (_context.Users.Any(u => u.UserName == requestUser.UserName))
+            {
+                return BadRequest("User with this username already exists.");
+            }
+
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(requestUser.Password);
+
+            var newUser = new User()
+            {
+                Name = requestUser.Name,
+                Surname = requestUser.Surname,
+                UserName = requestUser.UserName,
+                Email = requestUser.Email,
+                PasswordHash = passwordHash,
+                IsAdmin = requestUser.IsAdmin,
+                Reservations = new List<Reservation>()
+            };
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+
+            var userDto = new UserDto()
+            {
+                Id = newUser.Id,
+                Name = newUser.Name,
+                Surname = newUser.Surname,
+                UserName = newUser.UserName,
+                Email = newUser.Email,
+                Reservations = new List<ReservationDto>()
+            };
+
+            return Ok(userDto);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(LoginDto loginUser)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserName == loginUser.UserName);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(loginUser.Password, user.PasswordHash))
+            {
+                return BadRequest("Invalid username or password.");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,user.UserName),
+                new Claim(ClaimTypes.Role, user.IsAdmin ? "Admin" : "User")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+    }
+}
--- DeskBookingSystem/Program.cs
+++ DeskBookingSystem/Program.cs
@@ -1,5 +1,8 @@
 using DeskBookingSystem.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace DeskBookingSystem
 {
@@ -13,7 +16,51 @@ namespace DeskBookingSystem
             builder.Services.AddDbContext<BookingContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DeskBookingSystem API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "Bearer",
+                            Name = "Authorization",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+            // JWT Authentication
+            builder.Services.AddAuthentication("Bearer").AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Token"])),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
 
             var app = builder.Build();
 
@@ -21,11 +68,15 @@ namespace DeskBookingSystem
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "DeskBookingSystem API v1");
+                });
             }
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllers();
