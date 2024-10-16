7000803525c9bdd9e8804cec6af6b6e2ad4bffd3
Maciej Puchalski <maciejpe34@gmail.com>
2024-10-16 21:24:48 +0200

    Register all services and repositories

--- DeskBookingSystem/Configurations/ServiceConfiguration.cs
+++ DeskBookingSystem/Configurations/ServiceConfiguration.cs
@@ -8,8 +8,14 @@ namespace DeskBookingSystem.Configurations
         public static void ConfigureServices(this IServiceCollection services)
         {
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IReservationService, ReservationService>();
+            services.AddScoped<ILocationService, LocationService>();
+            services.AddScoped<IReservationService, ReservationService>();
 
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IDeskRepository, DeskRepository>();
+            services.AddScoped<ILocationRepository, LocationRepository>();
+            services.AddScoped<IReservationRepository, ReservationRepository>();
         }
     }
 }
