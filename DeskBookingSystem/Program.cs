5a5abc888a1a646c965d190cdd9f9638f3caad70
Maciej Puchalski <maciejpe34@gmail.com>
2024-10-16 21:25:31 +0200

    Add httpAccessor to builder, small change in .csproj

--- DeskBookingSystem/Program.cs
+++ DeskBookingSystem/Program.cs
@@ -17,6 +17,7 @@ namespace DeskBookingSystem
             builder.Services.AddDbContext<BookingContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddControllers();
             builder.Services.ConfigureServices();
+            builder.Services.AddHttpContextAccessor();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
             {
